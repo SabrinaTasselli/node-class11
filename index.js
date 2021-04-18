87d053607e175676fb3e975e14619b6740856a8d
Sabrina Tasselli <rodrigodandrea>
2021-04-18 16:07:31 -0300

    class11

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.use(express.json());
+
+app.use('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Accept, Origin, Content-Type, access_token');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
+function handleRegister(req, res){
+    user = req.body;
+    res.send("Success!");
+    console.log(user);
+};
+
+function handleLoging(req, res){
+    let userPassword = req.body.password;
+    if(user != null && user.password == userPassword){
+        res.send("Success!");
+    }else{
+        res.send("User not found");
+    }
+};
+
+app.post("/register", handleRegister);
+app.put("/login", handleLoging);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
